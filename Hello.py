45555ac6a3922378a784043a19a18c6c855030e0
mquanit2k4 <93365603+mquanit2k4@users.noreply.github.com>
2023-11-02 22:32:11 +0700

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,61 @@
+print('My', 'name' , 'is', "Quan" , sep='-' , end = "!\n")
+# Day la chu thich
+"""
+Chu thich
+tren nhieu 
+dong
+"""
+minh_quan = "Minh Quan"
+print(minh_quan)
+print(type(minh_quan))
+a = 100
+b = 100.2
+c = 3+5j
+print("Kieu du lieu cua a , b , c la: " , type(a) , type(b) , type(c))
+
+# Chuyen doi cac he ve co so 10
+a = 0b1011 
+b = 0o123 
+c=0x22A 
+print(a,b,c)
+
+# So thuc
+a = 3.5
+b = 3e5
+c = 1.9e308
+d = 5.4e-325
+print (a,b,c,d)
+
+a = 28.0435454
+print('%.2f' % a)
+print('{:.7}'.format(a))
+
+# So phuc
+j = 1+5j
+print(j.real , j.imag)
+
+# Kieu bool
+a = True
+b = False
+print(type(a))
+print(type(b))
+print(bool(100))
+print(bool(0))
+print(bool(""))
+print(bool("Minh Quan"))
+
+
+#Kieu xau ki tu (str)
+s = "Python"
+x = 'python'
+t = """Hoc machine 
+learning bang
+Python"""
+print(s,x,t)
+
+# Ep kieu
+a = int("122323")
+b = str("Wo ai ni")
+c = float("50")
+print (a,b,c)
+
